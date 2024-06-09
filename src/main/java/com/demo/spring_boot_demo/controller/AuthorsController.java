7306e99e28ab06b2ada1df94690eff2e434e250e
justinturikumwe <justin.turikumwe@azzera.com>
2024-06-08 23:45:39 -0400

    Create endpoint to fetch books by author id.

--- src/main/java/com/demo/spring_boot_demo/controller/AuthorsController.java
+++ src/main/java/com/demo/spring_boot_demo/controller/AuthorsController.java
@@ -3,10 +3,12 @@ package com.demo.spring_boot_demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.spring_boot_demo.model.Author;
+import com.demo.spring_boot_demo.model.Book;
 import com.demo.spring_boot_demo.repository.AuthorRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +32,26 @@ public class AuthorsController {
             return null;
         }
         return authors;
+    }
+
+    @RequestMapping("/{id}/books")
+    public List<Book> getBooksByAuthorId(@PathVariable Long id) {
+        log.info("Fetching books by author id...Started");
+        if (id == null) {
+            log.error("Author id is null");
+            return null;
+        }
+        List<Book> books = new ArrayList<>();
+        try {
+            Author author = authorRepository.findById(id).orElse(null);
+            if (author != null) {
+                books = author.getBooks();
+            }
+        } catch (Exception e) {
+            log.error("Error while fetching books by author id", e.getMessage());
+            return null;
+        }
+        return books;
     }
 
 }

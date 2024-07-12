485e7f1e6ddb4c870357d9036a43e37cb95f7fd9
justinturikumwe <justin.turikumwe@azzera.com>
2024-07-11 22:45:22 -0400

    fix:add java.util.List import in BookService.

--- src/main/java/com/demo/spring_boot_demo/service/BookService.java
+++ src/main/java/com/demo/spring_boot_demo/service/BookService.java
@@ -1,10 +1,14 @@
 package com.demo.spring_boot_demo.service;
 
+import java.util.List;
+
 import com.demo.spring_boot_demo.dto.BookRequestDTO;
 import com.demo.spring_boot_demo.model.Book;
 
 public interface BookService {
     public Book create(BookRequestDTO bookRequestDTO);
+
     public List<Book> findAll();
+
     public Book findById(Long id);
 }
